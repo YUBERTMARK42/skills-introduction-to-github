4f54ce00c67979d5400cf0892733b3ffa9e7ee25
YUBERT   STENNEDY     MARK42 <yubertstennedy@gmail.com>
2025-02-02 19:07:39 +0100

    Add files via upload

---
+++ Laço de repeticção For.py
@@ -0,0 +1,23 @@
+lista=["Yuri","Banana","Morango","Bola","24 ","Computador" ,"Miguel"]
+for item in lista:
+    print(item)
+nome = input("Tell me something:")
+for x in range(11):
+    print(f"{x + 1} {nome}")
+for x in range(2,20):
+    print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
